bcb6d2c8cff26c97d0890bdbe1967a799cca3d62
jastrab <jastrab.sk@gmail.com>
2024-06-26 14:32:56 +0200

    add gitignore upd sql create table

--- mysql/db/00_init.sql
+++ mysql/db/00_init.sql
@@ -75,20 +75,125 @@ CREATE TABLE leaflet (
 );
 
 CREATE TABLE leaflet_texts (
-    id INT NOT NULL AUTO_INCREMENT,
-    id_leaflet INT NOT NULL,
-    page INT,
-    `text` TEXT,
-    ocr_size SMALLINT,
-    `createDate` DATETIME,
+	id INT NOT NULL AUTO_INCREMENT,
+	id_leaflet INT NOT NULL,
+	page INT,
+	`text` TEXT,
+	ocr_size SMALLINT,
+	`createDate` DATETIME,
 
 	PRIMARY KEY (id), 
 	FOREIGN KEY (id_leaflet) REFERENCES leaflet (id)
 
 );
+-- OPEN HOURS TABLES --
+
+CREATE TABLE open_hours_days (
+	id INTEGER NOT NULL AUTO_INCREMENT primary key,
+	monday VARCHAR(128),
+	tuesday VARCHAR(128),
+	wednesday VARCHAR(128),
+	thursday VARCHAR(128),
+	friday VARCHAR(128),
+	saturday VARCHAR(128),
+	sunday VARCHAR(128),
+	dateCheck datetime,
+	dateCreate datetime,
+	dateUpdate datetime
+);
+
+
+CREATE TABLE open_hours_gps (
+	id INTEGER NOT NULL AUTO_INCREMENT primary key,
+	lat real,
+    lon real
+);
+
+-- Shopping center
+CREATE TABLE open_hours_sc_index (
+	id INTEGER NOT NULL AUTO_INCREMENT primary key,
+	name VARCHAR(256) NOT NULL,
+	title VARCHAR(256) NOT NULL,
+	id_open_hours_days INTEGER,
+    id_open_hours_gps INTEGER,
+	source VARCHAR(128),
+	url VARCHAR(256),
+	type CHAR(1),
+	city VARCHAR(128),
+	address VARCHAR(128),
+	slug VARCHAR(256),
+	UNIQUE KEY name (name),
+	FOREIGN KEY (id_open_hours_gps) REFERENCES open_hours_gps (id),
+	FOREIGN KEY (id_open_hours_days) REFERENCES open_hours_days (id)
+);
+
+CREATE TABLE open_hours_shop_index (
+    id INTEGER NOT NULL AUTO_INCREMENT primary key,
+    name VARCHAR(256) NOT NULL,
+    title VARCHAR(256) NOT NULL,
+    source VARCHAR(256),
+    url VARCHAR(256),
+    type CHAR(1),
+	UNIQUE KEY name (name)
+
+);
+
+CREATE TABLE open_hours_sc (
+    id INTEGER NOT NULL AUTO_INCREMENT primary key,
+    id_oh_sc_index INTEGER NOT NULL,
+    title VARCHAR(256),
+	id_oh_days INTEGER,
+    url VARCHAR(256),
+    url_web VARCHAR(256),
+    slug VARCHAR(256) NOT NULL,
+	FOREIGN KEY (id_oh_days) REFERENCES open_hours_days (id),
+	FOREIGN KEY (id_oh_sc_index) REFERENCES open_hours_sc_index (id)
+);
+
+CREATE TABLE open_hours_shop (
+    id INTEGER NOT NULL AUTO_INCREMENT primary key,
+    id_oh_shop_index INTEGER NOT NULL,
+	id_oh_days INTEGER,
+    city VARCHAR(128),
+    address VARCHAR(128),    
+    id_oh_gps INTEGER,
+    desc1 VARCHAR(128),
+    desc2 VARCHAR(128),
+    url VARCHAR(256),
+    slug VARCHAR(256) NOT NULL,
+	FOREIGN KEY (id_oh_shop_index) REFERENCES open_hours_shop_index (id),
+	FOREIGN KEY (id_oh_days) REFERENCES open_hours_days (id),
+	FOREIGN KEY (id_oh_gps) REFERENCES open_hours_gps (id)
+);
+
 
 
 CREATE UNIQUE INDEX idx_shop_name ON shop (name);
 CREATE UNIQUE INDEX idx_shop_typ_name ON shop_typ (name);
+
 CREATE INDEX idx_leaflet_title ON leaflet (title);
+CREATE INDEX idx_leaflet_startDate ON leaflet (startDate);
+CREATE INDEX idx_leaflet_endDate ON leaflet (endDate);
+
 CREATE INDEX idx_leaflet_text_id_leaflet ON leaflet_texts (id_leaflet);
+
+
+
+CREATE INDEX idx_open_hours_shop_index_name ON open_hours_shop_index (name);
+CREATE INDEX idx_open_hours_sc_index_name ON open_hours_sc_index (name);
+
+CREATE INDEX idx_open_hours_shop_slug ON open_hours_shop (slug);
+CREATE INDEX idx_open_hours_sc_slug ON open_hours_sc (slug);
+
+
+
+-- DROP INDEX idx_shop_name ON shop;
+-- DROP INDEX idx_shop_typ_name ON shop_typ;
+-- DROP INDEX idx_leaflet_title ON leaflet;
+-- DROP INDEX idx_leaflet_text_id_leaflet ON leaflet_texts;
+
+-- DROP INDEX idx_open_hours_sc_index_name ON open_hours_sc_index;
+-- DROP INDEX idx_open_hours_shop_index_name ON open_hours_shop_index;
+
+-- DROP INDEX idx_leaflet_startDate ON leaflet;
+-- DROP INDEX idx_leaflet_endDate ON leaflet;
